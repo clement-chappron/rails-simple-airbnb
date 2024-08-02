7587457bb2d1cd155a253e938088ad3846c58e51
clement-chappron <clement.chappron@gmail.com>
2024-08-02 08:24:31 +0400

    creation de la show

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# Flats
+
+Flat.destroy_all
+puts 'All flats detroyed'
+
+Flat.create!([
+  {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+  },
+  {
+  name: Faker::Emotion.noun,
+  address: Faker::Address.full_address,
+  description: Faker::Books::Dune.quote,
+  price_per_night: 300,
+  number_of_guests: 5
+  },
+  {
+  name: Faker::Emotion.noun,
+  address: Faker::Address.full_address,
+  description: Faker::Books::Dune.quote,
+  price_per_night: 300,
+  number_of_guests: 8
+  },
+  {
+  name: Faker::Emotion.noun,
+  address: Faker::Address.full_address,
+  description: Faker::Books::Dune.quote,
+  price_per_night: 175,
+  number_of_guests: 3
+  },
+  {
+  name: Faker::Emotion.noun,
+  address: Faker::Address.full_address,
+  description: Faker::Books::Dune.quote,
+  price_per_night: 150,
+  number_of_guests: 2
+  }
+])
+
+
+puts '5 flats created'
